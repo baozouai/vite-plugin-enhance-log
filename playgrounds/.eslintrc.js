1fb6c6fcb5b89698a0bb8e01160afc72f3d31d99
baozouai <baozouai@gmail.com>
2023-07-16 11:52:58 +0800

    chore: playground add eslint ignore

---
+++ playgrounds/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+  rules: {
+    'no-debugger': 'off',
+    'no-restricted-syntax': 'off',
+    'unused-imports/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+  },
+}
